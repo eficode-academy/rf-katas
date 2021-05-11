b828368f6bdc3b2ee36875826354573e55ff07e2
Aleksi Simell <aleksi.simell@eficode.com>
2021-05-11 16:09:25 +0300

    Ease validation on exercise 05

--- setup/05-negative_testing_rule.py
+++ setup/05-negative_testing_rule.py
@@ -1,12 +1,13 @@
 from rflint.common import KeywordRule, SuiteRule, TestRule, ERROR
 
-ALLOWED_KEYWORDS = [
-    "Open Browser To Login Page",
+MUST_KEYWORDS = [
     "Enter Username",
     "Enter Password",
     "Submit Login Form",
     "Verify That Error Page Is Visible"
 ]
+
+SETUP_KEYWORD = "Open Browser To Login Page"
 
 
 class KeywordImplementationRule05(KeywordRule):
@@ -14,7 +15,7 @@ class KeywordImplementationRule05(KeywordRule):
 
     def apply(self, keyword):
         report = False
-        if keyword.name == ALLOWED_KEYWORDS[1] or keyword.name == ALLOWED_KEYWORDS[2]:
+        if keyword.name == MUST_KEYWORDS[0] or keyword.name == MUST_KEYWORDS[1]:
             if len(keyword.settings) == 0 or (not "[Arguments]" in keyword.settings[0]):
                 report = True
         if report:
@@ -24,9 +25,29 @@ class TestCaseImplementation05(TestRule):
     severity = ERROR
 
     def apply(self, test):
+        default_message = "Check that you've implemented test case {} as instructed: ".format(test.name)
+        default_message += "{} is expected as a setup or part of the test. {} are required as part of the test".format(SETUP_KEYWORD, ", ".join(MUST_KEYWORDS))
         test_steps = []
         for step in test.steps:
             if len(step) > 1:
-                test_steps.append(step[1])
-        if test_steps != ALLOWED_KEYWORDS:
-            self.report(test, "Check that you've implemented test case {} as instructed, expected: {}".format(test.name, ", ".join(ALLOWED_KEYWORDS)), test.linenumber)
+                test_steps.append(step[1].lower().title())
+        is_report = False
+        if len(test_steps) == 5 and test_steps != [SETUP_KEYWORD] + MUST_KEYWORDS:
+            is_report = True
+        elif len(test_steps) == 4:
+            has_setup = False
+            setup = list(filter(lambda s: "test setup" in str(s).lower(), test.parent.settings))[0]
+            if SETUP_KEYWORD.lower() in str(setup).lower() and not setup.is_comment():
+                has_setup = True
+            if not has_setup:
+                for setting in test.settings:
+                    s = str(setting).lower()
+                    if SETUP_KEYWORD.lower() in s and "[setup]" in s:
+                        has_setup = True
+                        break
+
+            if not has_setup:
+                is_report = True
+
+        if is_report:
+            self.report(test, default_message, test.linenumber)
