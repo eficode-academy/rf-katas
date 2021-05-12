1130e5f4be7edbce32644ddb4a89c600f50f3454
Aleksi Simell <aleksi.simell@eficode.com>
2021-05-12 09:28:21 +0300

    Chill with keyword order and don't check case sensitively

--- setup/07-test_template_rule.py
+++ setup/07-test_template_rule.py
@@ -1,20 +1,20 @@
 from rflint.common import KeywordRule, SuiteRule, TestRule, ERROR
 
 ALLOWED_KEYWORDS = [
-    'Verify That Error Page Is Visible',
-    'Template Error Page Is Visible When Using Incorrect Credentials'
+    'Template Error Page Is Visible When Using Incorrect Credentials',
+    'Verify That Error Page Is Visible'
 ]
 
 class TestTemplateInUse(SuiteRule):
     severity = ERROR
-    
+
 
     def apply(self, suite):
         is_test_template = False
         for setting in suite.settings:
             if setting[0].lower() == "test template":
                 is_test_template = True
-                if setting[1]  != ALLOWED_KEYWORDS[1]:
+                if setting[1].lower().title()  != ALLOWED_KEYWORDS[0]:
                     self.report(suite, "Did you add correct keyword for test template?, expected: {}".format(ALLOWED_KEYWORDS[1]), 0)
         if not is_test_template:
             self.report(suite, "Did you remember to add Test Template option?", 0)
@@ -26,7 +26,7 @@ class KeywordNamesIn07(SuiteRule):
         keywords = []
         for keyword in suite.keywords:
             keywords.append(keyword.name)
-        if keywords != ALLOWED_KEYWORDS:
+        if sorted(keywords) != ALLOWED_KEYWORDS:
             self.report(suite, "Did you implement all keywords?, expected: {}".format(", ".join(ALLOWED_KEYWORDS)), 0)
 
 class TestTemplateImplementation(KeywordRule):
@@ -40,9 +40,12 @@ class TestTemplateImplementation(KeywordRule):
     ]
 
     def apply(self, keyword):
-        if keyword.name == ALLOWED_KEYWORDS[1]:
+        if keyword.name.lower().title() == ALLOWED_KEYWORDS[0]:
             steps = []
             for step in keyword.steps:
-                steps.append(step[1])
+                try:
+                    steps.append(step[1].lower().title())
+                except IndexError:
+                    pass
             if steps != self.ALLOWED_TEMPLATE_STEPS:
                 self.report(keyword, "Did you implement all needed steps for template?, expected: {}".format(", ".join(self.ALLOWED_TEMPLATE_STEPS)), keyword.linenumber)
